93424977eee0cdc41084945b1c54182ef266d5c8
arturosalazar <arturo.v.salazar.jr@gmail.com>
2021-04-10 02:17:07 -0500

    2 files modified

--- src/app/health-quote/health-quote.component.ts
+++ src/app/health-quote/health-quote.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HealthQuoteComponent implements OnInit {
 
+  /*Values we need to collect either from user or derived from user 
+
+  DOB
+  aptc_eligible??
+  has_mec????
+  utilizationlevel?
+  Age <- calc from DOB
+  income? <- seems required by the api
+  isParent
+  Weight?
+  Height?
+  Gender
+  Uses Tobacco
+  Zip <- get from prev component
+  State <- derive from zip 
+  Current Year <- just calc it
+
+  */
+  
+
   constructor() { }
 
   ngOnInit() {
