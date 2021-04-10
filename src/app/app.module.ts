b7bad6d72cfb834b3885dea9b0156ddf46f375c4
arturosalazar <arturo.v.salazar.jr@gmail.com>
2021-04-09 19:50:39 -0500

    Moved login to it's own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot([
+      {path: '', component: LoginComponent},
+    ])],
+  declarations: [AppComponent, HelloComponent, LoginComponent],
+  providers:[],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
