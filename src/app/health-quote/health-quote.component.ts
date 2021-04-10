98d8afd8dba924448ff24614ee79bb3693341c68
arturosalazar <arturo.v.salazar.jr@gmail.com>
2021-04-10 03:01:44 -0500

    added several form fields to the website

--- src/app/health-quote/health-quote.component.ts
+++ src/app/health-quote/health-quote.component.ts
@@ -1,30 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-health-quote',
-  templateUrl: './health-quote.component.html',
-  styleUrls: ['./health-quote.component.css']
+  selector: "app-health-quote",
+  templateUrl: "./health-quote.component.html",
+  styleUrls: ["./health-quote.component.css"]
 })
 export class HealthQuoteComponent implements OnInit {
-
-  age:number; //Must be an integer
+  age: number; //Must be an integer
   zipcode: number; //To be retrieved from prev component
   zipString: string; //Calculated from number zip - needed to request
   state: string; //calculate from Zip
   countryFips: string; //required, we will need to calculate
-  market: string; //Individual, SHOP (small business health option program), or Any 
-  income: number; //floating point value 
+  market: string = "Individual";
+  income: number; //floating point value
   isParent: boolean;
   isPregnant: boolean;
   uses_tobacco: boolean;
-  gender //enum - Male or Female
-  utilization_level //enum Low, Medium, or High
+  gender; //enum - Male or Female
+  utilization_level; //enum Low, Medium, or High
 
-  
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
