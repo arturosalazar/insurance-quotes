d2e737d469a59f612aed6722e1f6a81f98589a93
arturosalazar <arturo.v.salazar.jr@gmail.com>
2021-04-09 22:15:50 -0500

    update home menu to include capability to choose health, auto, or health and auto insurance options

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  insuranceType: string = "";
+  zipcode: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  setHealth() {
+    this.insuranceType = "health";
   }
 
+  setAuto() {
+    this.insuranceType = "auto";
+  }
+
+  setHealthAndAuto() {
+    this.insuranceType = "health-and-auto";
+  }
+
+  getInsuranceQuote() {
+    console.log("zip : " + this.zipcode + " path: " + this.insuranceType);
+  }
 }
