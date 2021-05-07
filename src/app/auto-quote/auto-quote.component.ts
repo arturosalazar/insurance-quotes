6f3775aa54c975755fc39f89a086430423e7b89a
Anna Shklover <ashklover@hawk.iit.edu>
2021-05-07 18:39:40 -0500

    edits for auto and health+auto

--- src/app/auto-quote/auto-quote.component.ts
+++ src/app/auto-quote/auto-quote.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auto-quote.component.css']
 })
 export class AutoQuoteComponent implements OnInit {
+  age: number; //Must be an integer
+  zipcode: number; //To be retrieved from prev component
+  zipString: string; //Calculated from number zip - needed to request
+  state: string; //calculate from Zip
+  countryFips: string; //required, we will need to calculate
+  make: string; // car make
+  model: string; // car model
+  year: number; // car year
+  vin: string;
+  miles: number; // yearly miles
+  gender; //enum - Male or Female
+  safety: // passive restraint system, anti-lock brakes, airbags
+  infractions: boolean; // do they have infractions on their driving record
+  utilization_level; //enum Low, Medium, or High
 
   constructor() { }
 
--- src/app/health-and-auto/health-and-auto.component.ts
+++ src/app/health-and-auto/health-and-auto.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./health-and-auto.component.css']
 })
 export class HealthAndAutoComponent implements OnInit {
+  // health
+  age: number; //Must be an integer
+  zipcode: number; //To be retrieved from prev component
+  zipString: string; //Calculated from number zip - needed to request
+  state: string; //calculate from Zip
+  countryFips: string; //required, we will need to calculate
+  market: string = "Individual";
+  income: number; //floating point value
+  isParent: boolean;
+  isPregnant: boolean;
+  uses_tobacco: boolean;
+  gender; //enum - Male or Female
+  health_utilization; //enum Low, Medium, or High
+
+  // auto
+  make: string; // car make
+  model: string; // car model
+  year: number; // car year
+  vin: string;
+  miles: number; // yearly miles
+  safety: // passive restraint system, anti-lock brakes, airbags
+  infractions: boolean; // do they have infractions on their driving record
+  auto_utilization; //enum Low, Medium, or High
+
 
   constructor() { }
 
